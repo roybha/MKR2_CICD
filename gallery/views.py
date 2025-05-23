d14c57cf878c48c5f31805fcec020553a214105a
Андрій Рубан <dombldor15@gmail.com>
2025-05-23 12:44:34 +0300

    made some changes in setiings  and views

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
+from django.shortcuts import get_object_or_404, render
+from .models import Image
+
+def gallery_view(request):
+    one_month_ago = timezone.now().date() - timedelta(days=30)
+    images = Image.objects.filter(created_date__gte=one_month_ago)
+    return render(request, 'gallery.html', {'images': images})
+
+
+def image_detail(request, image_id):
+    image = get_object_or_404(Image, id=image_id)
+    return render(request, 'image_detail.html', {'image': image})
