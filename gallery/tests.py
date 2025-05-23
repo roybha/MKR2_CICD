af4497abd9a94a956a45a74abd821d3c52132f44
Андрій Рубан <dombldor15@gmail.com>
2025-05-23 12:48:40 +0300

    add tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Category, Image
+from datetime import date
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_str(self):
+        category = Category.objects.create(name='Nature')
+        self.assertEqual(str(category), 'Nature')
+
+class ImageModelTest(TestCase):
+    def test_str(self):
+        image = Image.objects.create(
+            title='Test Image',
+            created_date=date.today(),
+            age_limit=12
+        )
+        self.assertEqual(str(image), 'Test Image')
+
