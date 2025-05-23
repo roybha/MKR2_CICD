b20698fed2596c678229982f28bf3f5ef47a8a45
Андрій Рубан <dombldor15@gmail.com>
2025-05-23 12:55:34 +0300

    add templates and tests for them

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Category, Image
-from datetime import date
+from datetime import date, timedelta
+from django.urls import reverse
 
 class CategoryModelTest(TestCase):
     def test_str(self):
@@ -16,3 +17,29 @@ class ImageModelTest(TestCase):
         )
         self.assertEqual(str(image), 'Test Image')
 
+class GalleryViewTest(TestCase):
+    def test_gallery_shows_recent_images(self):
+        recent_image = Image.objects.create(
+            title='Recent',
+            created_date=date.today(),
+            age_limit=12
+        )
+        old_image = Image.objects.create(
+            title='Old',
+            created_date=date.today() - timedelta(days=60),
+            age_limit=12
+        )
+        response = self.client.get(reverse('gallery'))
+        self.assertContains(response, 'Recent')
+        self.assertNotContains(response, 'Old')
+
+class ImageDetailViewTest(TestCase):
+    def test_image_detail_view(self):
+        image = Image.objects.create(
+            title='Detail View',
+            created_date=date.today(),
+            age_limit=12
+        )
+        response = self.client.get(reverse('image_detail', args=[image.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Detail View')
